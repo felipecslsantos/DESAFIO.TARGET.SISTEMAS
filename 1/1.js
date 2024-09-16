ee4673abe0fee324218ff52fd821b8d3dd5fd181
Felipe <56681097+felipecslsantos@users.noreply.github.com>
2024-09-16 17:59:03 -0300

    DESAFIO.TARGET.SISTEMAS
    Commit initial

---
+++ 1/1.js
@@ -0,0 +1,18 @@
+/* 
+    1) Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0;
+    Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
+    Imprimir(SOMA);
+    Ao final do processamento, qual será o valor da variável SOMA?
+*/
+
+var INDICE = 13;
+var SOMA = 0;
+K = 0;
+
+K = K + 1;
+SOMA = SOMA + K;
+console.log(SOMA);
+
+/*
+    SOMA é igual a 1.
+*/    
