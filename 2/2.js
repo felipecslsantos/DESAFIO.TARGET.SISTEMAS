00d3958a468d55f3901cbaf885b1c01b4ad636fe
Felipe <56681097+felipecslsantos@users.noreply.github.com>
2024-10-02 17:51:52 -0300

    Resolução do 2º algoritmo.

--- 2/2.js
+++ 2/2.js
@@ -2,10 +2,25 @@
     2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
 */
 
-var numero = 2;
-var i = 0;
+var primeiroTermo = 0;
+var segundoTermo = 1;
+var numero = primeiroTermo + segundoTermo;
+
+console.log(primeiroTermo);
+console.log(segundoTermo);
+console.log(numero);
+
+primeiroTermo ++;
+segundoTermo ++;
+
+console.log(segundoTermo);
+
+var i;
+
 
 for(i = 0; i < 10; i++){
-    //numero = numero + 1;
+    numero = primeiroTermo + segundoTermo;
     console.log(numero);
+    primeiroTermo = segundoTermo;
+    segundoTermo = numero;
 }               
